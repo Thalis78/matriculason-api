6b5a2e022e8c5ab086b9881442a12dd42fcec2da
Thalisson <thalissonmoura138@gmail.com>
2025-04-04 12:47:07 -0300

    :recycle:refactor: altera implementação da migration V05_atualizacao_estrutural_banco

--- src/main/resources/db/migration/V05__atualizacao_estrutural_banco.sql
+++ src/main/resources/db/migration/V05__atualizacao_estrutural_banco.sql
@@ -1,21 +1,25 @@
 ALTER TABLE usuarios
-    ALTER COLUMN senha TYPE VARCHAR(12);
-
-ALTER TABLE administradores DROP CONSTRAINT IF EXISTS UK16vb32dwfq32vxgxd6k6awqrq;
-ALTER TABLE administradores DROP CONSTRAINT IF EXISTS UKmims61qt1j972b3ggvn2dgl5c;
+    MODIFY COLUMN senha VARCHAR(12) NOT NULL;
 
 
-ALTER TABLE alunos
-    ADD CONSTRAINT fk_aluno_usuario
-    FOREIGN KEY (usuario_id)
-    REFERENCES usuarios(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+SET @idx_name = (SELECT INDEX_NAME
+                 FROM INFORMATION_SCHEMA.STATISTICS
+                 WHERE TABLE_NAME = 'administradores'
+                 AND INDEX_NAME = 'UK16vb32dwfq32vxgxd6k6awqrq');
 
-ALTER TABLE administradores
-    ADD CONSTRAINT fk_admin_usuario
-    FOREIGN KEY (usuario_id)
-    REFERENCES usuarios(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+SET @sql = IF(@idx_name IS NOT NULL, 'ALTER TABLE administradores DROP INDEX UK16vb32dwfq32vxgxd6k6awqrq', 'SELECT 1');
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+SET @idx_name2 = (SELECT INDEX_NAME
+                 FROM INFORMATION_SCHEMA.STATISTICS
+                 WHERE TABLE_NAME = 'administradores'
+                 AND INDEX_NAME = 'UKmims61qt1j972b3ggvn2dgl5c');
+
+SET @sql2 = IF(@idx_name2 IS NOT NULL, 'ALTER TABLE administradores DROP INDEX UKmims61qt1j972b3ggvn2dgl5c', 'SELECT 1');
+PREPARE stmt2 FROM @sql2;
+EXECUTE stmt2;
+DEALLOCATE PREPARE stmt2;
+
 
